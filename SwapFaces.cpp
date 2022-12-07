28f7d89813e100bb1671b50692a4d8b98b5a5746
t0nic0 <tonicoric68@googlemail.com>
2022-12-07 16:58:37 +0100

    Gesichtserkennung Implementiert

--- SwapFaces.cpp
+++ SwapFaces.cpp
@@ -1,45 +1,37 @@
-#include <opencv2/core.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
-#include <iostream>
-#include <stdio.h>
+#include<iostream>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+//This header includes definition of 'rectangle()' function//
+#include<opencv2/objdetect/objdetect.hpp>
+//This header includes the definition of Cascade Classifier//
+#include<string>
 
-using namespace cv;
 using namespace std;
-int main(int, char**)
-{
-    Mat frame;
-    //--- INITIALIZE VIDEOCAPTURE
-    VideoCapture cap;
-    // open the default camera using default API
-    // cap.open(0);
-    // OR advance usage: select any API backend
-    int deviceID = 0;             // 0 = open default camera
-    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-    // open selected camera using selected API
-    cap.open(deviceID, apiID);
-    // check if we succeeded
-    if (!cap.isOpened()) {
-        cerr << "ERROR! Unable to open camera\n";
-        return -1;
-    }
-    //--- GRAB AND WRITE LOOP
-    cout << "Start grabbing" << endl
-        << "Press any key to terminate" << endl;
-    for (;;)
-    {
-        // wait for a new frame from camera and store it into 'frame'
-        cap.read(frame);
-        // check if we succeeded
-        if (frame.empty()) {
-            cerr << "ERROR! blank frame grabbed\n";
+using namespace cv;
+int main(int argc, char** argv) {
+    Mat video_stream;//Declaring a matrix hold frames from video stream//
+    VideoCapture real_time(0);//capturing video from default webcam//
+    namedWindow("Face Detection");//Declaring an window to show the result//
+    string trained_classifier_location = "C:/Users/Toni/Documents/Workspace/opencv/sources/data/haarcascades/haarcascade_frontalface_alt.xml";//Defining the location our XML Trained Classifier in a string//
+    CascadeClassifier faceDetector;//Declaring an object named 'face detector' of CascadeClassifier class//
+    faceDetector.load(trained_classifier_location);//loading the XML trained classifier in the object//
+    vector<Rect>faces;//Declaring a rectangular vector named faces//
+    while (true) {
+        faceDetector.detectMultiScale(video_stream, faces, 1.1, 4, CASCADE_SCALE_IMAGE, Size(30, 30));//Detecting the faces in 'image_with_humanfaces' matrix//
+        real_time.read(video_stream);// reading frames from camera and loading them in 'video_stream' Matrix//
+        for (int i = 0; i < faces.size(); i++) { //for locating the face
+            Mat faceROI = video_stream(faces[i]);//Storing face in the matrix//
+            int x = faces[i].x;//Getting the initial row value of face rectangle's starting point//
+            int y = faces[i].y;//Getting the initial column value of face rectangle's starting point//
+            int h = y + faces[i].height;//Calculating the height of the rectangle//
+            int w = x + faces[i].width;//Calculating the width of the rectangle//
+            rectangle(video_stream, Point(x, y), Point(w, h), Scalar(255, 0, 255), 2, 8, 0);//Drawing a rectangle using around the faces//
+        }
+        imshow("Face Detection", video_stream);
+        //Showing the detected face//
+        if (waitKey(10) == 27) { //wait time for each frame is 10 milliseconds//
             break;
         }
-        // show live and wait for a key with timeout long enough to show images
-        imshow("Live", frame);
-        if (waitKey(5) >= 0)
-            break;
     }
-    // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
