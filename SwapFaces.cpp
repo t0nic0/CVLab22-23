a706f1c7600832c81ec1bbcd99e7e2b895299af4
t0nic0 <tonicoric68@googlemail.com>
2023-01-18 12:27:31 +0100

    Verbessert mit ear

--- SwapFaces.cpp
+++ SwapFaces.cpp
@@ -11,100 +11,94 @@
 using namespace std;
 using namespace cv;
 
-int main(int argc, char** argv) {
-
-    // Load the cascades
-    CascadeClassifier face_cascade;
+int main()
+{
+    // Load the cascade classifier model
+    cv::CascadeClassifier face_cascade;
     face_cascade.load("C:/Users/Toni/Documents/Workspace/opencv/sources/data/lbpcascades/lbpcascade_frontalface_improved.xml");
-    CascadeClassifier eyes_cascade;
-    eyes_cascade.load("C:/Users/Toni/Documents/Workspace/opencv/sources/data/haarcascades/haarcascade_eye.xml");
 
-    // Open the video stream
-    VideoCapture stream(0);
-    if (!stream.isOpened()) {
-        cout << "Error : Unable to open camera" << endl;
-        return -1;
-    }
+    cv::CascadeClassifier eye_cascade;
+    eye_cascade.load("C:/Users/Toni/Documents/Workspace/opencv/sources/data/haarcascades/haarcascade_eye.xml");
 
-    // Variables to track eye blinks
-    int blink_count1 = 0;
-    int blink_count2 = 0;
-    double prev_norm1 = 0;
-    double prev_norm2 = 0;
-    double curr_norm1 = 0;
-    double curr_norm2 = 0;
-    double threshold = 0.02;
-    bool face1_detected = false;
-    bool face2_detected = false;
+    // Open the webcam
+    cv::VideoCapture cap(0);
 
-    while (true) {
-        Mat frame;
-        stream >> frame;
+    int blink_counter = 0;
+    double ear_threshold = 0.5;
+
+    while (true)
+    {
+        cv::Mat frame;
+        cap >> frame;
 
         // Convert to grayscale
-        Mat gray;
-        cvtColor(frame, gray, COLOR_BGR2GRAY);
+        cv::Mat gray;
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
 
         // Detect faces
-        vector<Rect> faces;
-        face_cascade.detectMultiScale(gray, faces, 1.1, 3, 0, Size(30, 30));
+        std::vector<cv::Rect> faces;
+        face_cascade.detectMultiScale(gray, faces);
 
-        // Draw rectangles around the faces and eyes
-        for (size_t i = 0; i < faces.size(); i++) {
-            rectangle(frame, faces[i], Scalar(255, 0, 0), 2);
+        // Loop through the detected faces
+        for (const auto& face : faces)
+        {
+            // Detect eyes in the face region
+            std::vector<cv::Rect> eyes;
+            eye_cascade.detectMultiScale(gray(face), eyes);
 
-            Mat faceROI = gray(faces[i]);
-            vector<Rect> eyes;
+            for (const auto& eye : eyes)
+            {
+                // Create a rectangle for the eye
+                cv::Rect eye_rect(face.x + eye.x, face.y + eye.y, eye.width, eye.height);
 
-            //-- In each face, detect eyes
-            eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+                // Extract the eye region
+                cv::Mat eye_region = gray(eye_rect);
 
-            for (size_t j = 0; j < eyes.size(); j++) {
-                Point center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
-                int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25);
-                circle(frame, center, radius, Scalar(255, 0, 0), 2);
+                // Detect the eyes' landmarks
+                vector<Point2f> landmarks;
+                cv::goodFeaturesToTrack(eye_region, landmarks, 8, 0.1, 10);
 
-                // Count eye blinks for the first face
-                if (i == 0) {
-                    Mat eyeROI = gray(Rect(faces[i].x + eyes[j].x, faces[i].y + eyes[j].y, eyes[j].width, eyes[j].height));
-                    Mat eyeROI_norm;
-                    cv::normalize(eyeROI, eyeROI_norm, 0, 255, cv::NORM_MINMAX);
-                    curr_norm1 = cv::norm(eyeROI_norm, NORM_L1);
-                    if (prev_norm1 == 0) prev_norm1 = curr_norm1;
-                    else {
-                        double norm_diff = abs(prev_norm1 - curr_norm1);
-                        if (norm_diff > threshold) {
-                            blink_count1++;
-                        }
-                        prev_norm1 = curr_norm1;
-                    }
-                }
+                // Calculate the Eye Aspect Ratio (EAR)
+                if(landmarks.size() >= 6) {
+                  double a = cv::norm(landmarks[1] - landmarks[5]);
+                  double b = cv::norm(landmarks[2] - landmarks[4]);
+                  double c = cv::norm(landmarks[0] - landmarks[3]);
+                  double ear = (a + b) / (2 * c);
 
-                // Count eye blinks for the second face
-                else if (i == 1) {
-                    Mat eyeROI = gray(Rect(faces[i].x + eyes[j].x, faces[i].y + eyes[j].y, eyes[j].width, eyes[j].height));
-                    Mat eyeROI_norm;
-                    cv::normalize(eyeROI, eyeROI_norm, 0, 255, cv::NORM_MINMAX);
-                    curr_norm2 = cv::norm(eyeROI_norm, NORM_L1);
-                    if (prev_norm2 == 0) prev_norm2 = curr_norm2;
-                    else {
-                        double norm_diff = abs(prev_norm2 - curr_norm2);
-                        if (norm_diff > threshold) {
-                            blink_count2++;
-                        }
-                        prev_norm2 = curr_norm2;
-                    }
+                  cout << "ear: " << ear << " ";
+
+                  // Check if the eye is closed (EAR is low)
+                  if (ear < ear_threshold)
+                  {
+                      // Increment the blink counter
+                      blink_counter++;
+
+                      // Draw a rectangle around the closed eye
+                      cv::rectangle(frame, eye_rect, cv::Scalar(0, 0, 255), 2);
+                  }
+                  else
+                  {
+                      // Draw a rectangle around the open eye
+                      cv::rectangle(frame, eye_rect, cv::Scalar(0, 255, 0), 2);
+                  }
                 }
             }
         }
 
-        // Display blink count for each face
-        putText(frame, "Blink count (face 1): " + to_string(blink_count1), Point(10, 30), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0, 0, 255), 2);
-        putText(frame, "Blink count (face 2): " + to_string(blink_count2), Point(10, 60), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0, 0, 255), 2);
+        // Display the blink counter on the frame
+        cv::putText(frame, "Blinks: " + std::to_string(blink_counter), cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 255), 2);
+
         // Show the frame
-        imshow("Frame", frame);
-        if (waitKey(1) == 27) break;
+        cv::imshow("Frame", frame);
+
+        // Exit if the user presses the 'Esc' key
+        if (cv::waitKey(1) == 27)
+            break;
     }
+
+    // Release the webcam and close the window
+    cap.release();
+    cv::destroyAllWindows();
 
     return 0;
 }
