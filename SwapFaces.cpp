6a01ffba13b10fb8d486b76637d2afe228e61494
Paul Kilger <paul.kilger@siemens.com>
2023-01-18 12:48:32 +0100

    working state of face recognition and smile recognition

--- SwapFaces.cpp
+++ SwapFaces.cpp
@@ -33,8 +33,23 @@ int main(int argc, char** argv) {
             int h = y + faces[i].height;//Calculating the height of the rectangle//
             int w = x + faces[i].width;//Calculating the width of the rectangle//
             rectangle(video_stream, Point(x, y), Point(w, h), Scalar(255, 0, 255), 2, 8,0);//Drawing a rectangle using around the faces//
-            //TODO: FaceSwap evtl. mit ellyptischer Form umbauen
-            //ellipse(video_stream, Point(x , y ), Size(h/2, w/2), 0, 0, 360, Scalar(255, 0, 255), 8,0);
+            //Detect the mouth
+            string trained_classifier_location_smile = "D:/UserData/z0047zpj/ComputerVisionProjekt/opencv/sources/data/haarcascades/haarcascade_smile.xml";//Defining the location our XML Trained Classifier in a string// 
+            CascadeClassifier smileDetector;//Declaring an object named 'face detector' of CascadeClassifier class//
+            smileDetector.load(trained_classifier_location_smile);//loading the XML trained classifier in the object//
+            vector<Rect>smiles;//Declaring a rectangular vector named faces//
+            smileDetector.detectMultiScale(faceROI, smiles, 1.1, 4, CASCADE_SCALE_IMAGE, Size(30, 30));//Detecting the faces in 'image_with_humanfaces' matrix//
+            for (int i = 0; i < smiles.size(); i++) { //for locating the smile
+                Mat smileROI = faceROI(smiles[i]);//Storing smile in the matrix//
+                int x = smiles[i].x;//Getting the initial row value of face rectangle's starting point//
+                int y = smiles[i].y;//Getting the initial column value of face rectangle's starting point//
+                int h = y + smiles[i].height;//Calculating the height of the rectangle//
+                int w = x + smiles[i].width;//Calculating the width of the rectangle//
+                rectangle(faceROI, Point(x, y), Point(w, h), Scalar(255, 0, 255), 2, 8, 0);//Drawing a rectangle using around the mouths//
+
+            }
+
+            
         }
         Mat display;
         video_stream.copyTo(display);
@@ -43,8 +58,6 @@ int main(int argc, char** argv) {
             Mat faceROI1 = display(faces[0]);
             Mat faceROI2 = display(faces[1]);//Storing face in the matrix//
 
-            //Mat faceROI1 = video_stream(faces[0]);
-            //Mat faceROI2 = video_stream(faces[1]);//Storing face in the matrix//
     
 
             // Define corner points of faces in cevtor
@@ -64,29 +77,6 @@ int main(int argc, char** argv) {
             vector<Point2f> points1{ p1_1,p1_2,p1_3,p1_4 };
             vector<Point2f> points2{ p2_1,p2_2,p2_3,p2_4 };
 
-            //TEST SMILE RECOGNITION
-            Mat gray;
-            faceROI1.copyTo(gray);
-            //cv::cvtColor(faceROI1,gray, cv::COLOR_BGR2GRAY);
-            string trained_classifier_location_smile = "D:/UserData/z0047zpj/ComputerVisionProjekt/opencv/sources/data/haarcascades/haarcascade_smile.xml";//Defining the location our XML Trained Classifier in a string// 
-            CascadeClassifier smileDetector;//Declaring an object named 'face detector' of CascadeClassifier class//
-            smileDetector.load(trained_classifier_location_smile);//loading the XML trained classifier in the object//
-            vector<Rect>smiles;//Declaring a rectangular vector named faces//
-            smileDetector.detectMultiScale(gray, smiles, 1.1, 4, CASCADE_SCALE_IMAGE, Size(30, 30));//Detecting the faces in 'image_with_humanfaces' matrix//
-
-            for (int i = 0; i < smiles.size(); i++) { //for locating the face
-                Mat smileROI = gray(smiles[i]);//Storing face in the matrix//
-                int x = smiles[i].x;//Getting the initial row value of face rectangle's starting point//
-                int y = smiles[i].y;//Getting the initial column value of face rectangle's starting point//
-                int h = y + smiles[i].height;//Calculating the height of the rectangle//
-                int w = x + smiles[i].width;//Calculating the width of the rectangle//
-                rectangle(gray, Point(x, y), Point(w, h), Scalar(255, 0, 255), 2, 8, 0);//Drawing a rectangle using around the faces//
-                imshow("Smile Detection", gray);
-                //TODO: FaceSwap evtl. mit ellyptischer Form umbauen
-                //ellipse(video_stream, Point(x , y ), Size(h/2, w/2), 0, 0, 360, Scalar(255, 0, 255), 8,0);
-            }
-
-
 
             //find Homography matrix that is the homography from face 2 to face 1
             Mat h1 = findHomography(points2, points1);
@@ -105,113 +95,16 @@ int main(int argc, char** argv) {
             //Warp faceROI1 and save it in im_out2
             warpPerspective(faceROI1, im_out2, h2, faceROI2.size());
 
-            //Show results 
-            //imshow("Source Image", faceROI2);
-            //imshow("Destination Image", faceROI1);
-            //imshow("Warped Source Image", im_out1);
+           
             cout << faceROI1.size()<<endl;
             cout << im_out1.size()<<endl;
             cout << "these numbers should be equal " << endl;
-            
-
-            //TODO: position new image im_out at the right place in the videostream
-            // 
             
 
             //copyTo for adding the image im_out to the videstream
             im_out1.copyTo(faceROI1);
             //copyTo for adding the image im_out to the videstream
             im_out2.copyTo(faceROI2);
-
-            
-            //Display images
-
-
-            /*cv::resize(faceROI1, faceROI1temp, cv::Size(faceROI2.size().width, faceROI2.size().height));
-            cv::resize(faceROI2, faceROI2temp, cv::Size(faceROI1.size().width, faceROI1.size().height));
-            faceROI2temp.copyTo(faceROI1);
-            faceROI1temp.copyTo(faceROI2);*/
-
-
-
-
-            /*
-            // Create a rough mask around the airplane.
-            Mat faceROI1_mask = Mat::zeros(faceROI1temp.rows, faceROI1temp.cols, faceROI1temp.depth());
-
-            // Define the mask as a closed polygon
-            Point poly1[1][7];
-            poly1[0][0] = Point(4, 80);
-            poly1[0][1] = Point(30, 54);
-            poly1[0][2] = Point(151, 63);
-            poly1[0][3] = Point(254, 37);
-            poly1[0][4] = Point(298, 90);
-            poly1[0][5] = Point(272, 134);
-            poly1[0][6] = Point(43, 122);
-
-            const Point* polygons1[1] = { poly1[0] };
-            int num_points1[] = { 7 };
-
-            // Create mask by filling the polygon
-
-            fillPoly(faceROI1_mask, polygons1, num_points1, 1, Scalar(255, 255, 255));
-
-            // Create a rough mask around the airplane.
-            Mat faceROI2_mask = Mat::zeros(faceROI2temp.rows, faceROI2temp.cols, faceROI2temp.depth());
-
-            // Define the mask as a closed polygon
-            Point poly2[1][7];
-            poly2[0][0] = Point(4, 80);
-            poly2[0][1] = Point(30, 54);
-            poly2[0][2] = Point(151, 63);
-            poly2[0][3] = Point(254, 37);
-            poly2[0][4] = Point(298, 90);
-            poly2[0][5] = Point(272, 134);
-            poly2[0][6] = Point(43, 122);
-
-            const Point* polygons2[1] = { poly2[0] };
-            int num_points2[] = { 7 };
-
-            // Create mask by filling the polygon
-
-            fillPoly(faceROI2_mask, polygons2, num_points2, 1, Scalar(255, 255, 255));
-
-            cv::Mat output;
-            Point2f center(faceROI1temp.cols/2, faceROI1temp.rows/ 2);
-            cv::seamlessClone(faceROI1temp, faceROI2, faceROI1_mask, center, output,NORMAL_CLONE);
-            imshow("Faces",output);
-
-
-            /*
-            Point2f p1_1(faces[0].x, faces[0].y);
-            Point2f p1_2(faces[0].x + faces[0].width, faces[0].y);
-            Point2f p1_3(faces[0].x, faces[0].y +  faces[0].height);
-            Point2f p1_4(faces[0].x + faces[0].width, faces[0].y + faces[0].height);
-
-            Point2f p2_1(faces[1].x, faces[1].y);
-            Point2f p2_2(faces[1].x + faces[1].width, faces[1].y);
-            Point2f p2_3(faces[1].x, faces[1].y + faces[1].height);
-            Point2f p2_4(faces[1].x + faces[1].width, faces[1].y + faces[1].height);
-
-            vector<Point2f> points1{p1_1,p1_2,p1_3,p1_4};
-            vector<Point2f> points2{p2_1,p2_2,p2_3,p2_4};
-            Mat h = cv::findHomography(points1, points2, cv::RANSAC);
-            cout << "H:\n" << h << endl;
-            warpPerspective(faceROI1src, faceROI2src, h, faceROI2src.size());
-
-            Mat img_draw_matches;
-            hconcat(faceROI1src, faceROI2src, img_draw_matches);
-            for (size_t i = 0; i < points1.size(); i++)
-            {
-                Mat pt1 = (Mat_<double>(3, 1) << points1[i].x, points1[i].y, 1);
-                Mat pt2 = h * pt1;
-                pt2 /= pt2.at<double>(2);
-                Point end((int)(faceROI1src.cols + pt2.at<double>(0)), (int)pt2.at<double>(1));
-                line(img_draw_matches, points1[i], end, 23, 2);
-            }
-            imshow("Draw matches", img_draw_matches);
-
-           */
 
            
         }
